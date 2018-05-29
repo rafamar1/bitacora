d5c62ff7d83c98bef04e6877395763e92ad3fb59
Rafa <Rafa@RafaMar.Home>
2018-05-29 07:13:47 +0200

    -Jars y archivos de configuración 28/05

--- bitacora/web/resources/js/formRegistro_validate.js
+++ bitacora/web/resources/js/formRegistro_validate.js
@@ -1,39 +1,54 @@
 $(function () {
+
+    $.validator.addMethod('passwordMatch', function (value, element) {
+        // The two password inputs
+        var password = $("#password").val();
+        var confirmPassword = $("#password2").val();
+        console.log("hola amigito");
+        // Check for equality with the password inputs
+        if (password !== confirmPassword) {
+            return false;
+        } else {
+            return true;
+        }
+    }, "Your Passwords Must Match");
+
     $('#formRegistro').validate({
         rules: {
             nickname: {required: true, minlength: 4},
             nombre: {required: true, minlength: 4},
             password: {required: true, minlength: 6},
-            password2: {required: true, minlength: 6},
+            password2: {required: true, minlength: 6, passwordMatch: true},
             mail: {
                 required: true,
                 email: true,
                 /*remote: {
-                    url: "compruebaMailRemote",
-                    type: "post",
-                    data: {mail: function () {
-                            return $("#mail").val();
-                        }
-                    }
-
-                }*/
+                 url: "compruebaMailRemote",
+                 type: "post",
+                 data: {mail: function () {
+                 return $("#mail").val();
+                 }
+                 }
+                 
+                 }*/
             }
         },
 
         messages: {
             nickname: {required: "El nombre de usuario es obligatorio",
-                remote:"Ese nombre de usuario no está disponible",
-                minlength:"Indique un usuario de al menos 4 caractres"},
+                remote: "Ese nombre de usuario no está disponible",
+                minlength: "Indique un usuario de al menos 4 caracteres"},
             nombre: {required: "El nombre es obligatorio",
-                minlength:"Indique un nombre de al menos 4 caractres"},
+                minlength: "Indique un nombre de al menos 4 caracteres"},
             password: {required: "El campo contraseña es obligatorio",
-                minlength:"La contraseña debe tener al menos 6 caracteres"},
+                minlength: "La contraseña debe tener al menos 6 caracteres"},
             password2: {required: "Repita la contraseña",
-                minlength:"La contraseña debe tener al menos 6 caracteres"},
-            mail: { required: "El campo email es obligatorio",
-                    email: "El formato de correo introducido no es válido (usuario@dominio.es)",
-                    remote: "El correo indicado ya está en uso, utilice uno diferente"
-                    }            
+                minlength: "La contraseña debe tener al menos 6 caracteres",
+                passwordMatch: "Las contraseñas deben coincidir"},
+            mail: {required: "El campo email es obligatorio",
+                email: "El formato de correo introducido no es válido (usuario@dominio.es)",
+                remote: "El correo indicado ya está en uso, utilice uno diferente"
+            }
         }
     });
 });
